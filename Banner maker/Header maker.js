79319e957bf7b464a00a73bd79fa952457b595b1
johnboss12 <trevor.lockwich2@d11.org>
2025-09-23 13:31:52 -0600

    Added auto color scheme picker to banner maker

--- Banner maker/Header maker.js
+++ Banner maker/Header maker.js
@@ -6,25 +6,43 @@ var Logo_link; //= "https://resources.finalsite.net/images/f_auto,q_auto,t_image
 var School_slogan; //="where learning happens beyond four walls";
 var School_name;//="Howbert Elementary";
 var out = '<div style="overflow:hidden;height:fit-content"><h1 style="text-align:center;">'+School_name+'</h1>\n<div style="text-align:right;display:block;">\n<img src="'+Logo_link+'" style="min-width:10%;max-width:40%;float:right;padding-right:10%;" alt="'+School_name+' Logo"></div>\n<div style="min-width:70%;">\n<h2 style="text-align:center;"> '+School_slogan+'</h2></div><div>';
+var theme = 0;
+var themeCount = 10;
+
+
+function themeUp(){if(theme==themeCount-1){theme=0;}theme++;colorCheck();}
+
+function colorCheck(){
+const colorThief = new ColorThief();
+const img = document.querySelector('img');
+img.crossOrigin = 'Anonymous';
+console.log(img);
+ if (img.complete) {
+      var color = colorThief.getPalette(img,themeCount);
+        console.log(color);
+         preview.style.border = "5px solid rgb("+color[theme][0]+","+color[theme][1]+","+color[theme][2]+")";
+    } else {
+      img.addEventListener('load', function() {
+        var color = colorThief.getPalette(img,themeCount);
+        console.log(color);
+         preview.style.border = "5px solid rgb("+color[theme][0]+","+color[theme][1]+","+color[theme][2]+")";
+      });
+    }
+
+   
+}
 
 function copy() {
   navigator.clipboard.writeText(out);
-
-
 }
-
-
-
-
-
-
 
 
 url.addEventListener('change', (event) => {
         Logo_link = url.value
         out = '<div style="overflow:hidden;height:fit-content"><h1 style="text-align:center;">'+School_name+'</h1>\n<div style="text-align:right;display:block;">\n<img src="'+Logo_link+'" style="min-width:10%;max-width:40%;float:right;padding-right:10%;" alt="'+School_name+' Logo"></div>\n<div style="min-width:70%;">\n<h2 style="text-align:center;"> '+School_slogan+'</h2></div><div>';
         preview.innerHTML=out;
-        text.innerText=out;
+        text.innerText=out; 
+        colorCheck();
         
     });
 title.addEventListener('change', (event) => {
@@ -32,6 +50,7 @@ title.addEventListener('change', (event) => {
         out = '<div style="overflow:hidden;height:fit-content"><h1 style="text-align:center;">'+School_name+'</h1>\n<div style="text-align:right;display:block;">\n<img src="'+Logo_link+'" style="min-width:10%;max-width:40%;float:right;padding-right:10%;" alt="'+School_name+' Logo"></div>\n<div style="min-width:70%;">\n<h2 style="text-align:center;"> '+School_slogan+'</h2></div><div>';
         preview.innerHTML=out;
         text.innerText=out;
+        colorCheck();
         
     });
 slogan.addEventListener('change', (event) => {
@@ -39,7 +58,7 @@ slogan.addEventListener('change', (event) => {
         out = '<div style="overflow:hidden;height:fit-content"><h1 style="text-align:center;">'+School_name+'</h1>\n<div style="text-align:right;display:block;">\n<img src="'+Logo_link+'" style="min-width:10%;max-width:40%;float:right;padding-right:10%;" alt="'+School_name+' Logo"></div>\n<div style="min-width:70%;">\n<h2 style="text-align:center;"> '+School_slogan+'</h2></div><div>';
         preview.innerHTML=out;
         text.innerText=out;
-        
+        colorCheck();
     });
 
 

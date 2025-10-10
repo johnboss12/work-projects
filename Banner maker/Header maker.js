44a71564c66092b4c8a4eb725b13a81ad9fc6e33
johnboss12 <trevor.lockwich2@d11.org>
2025-10-10 12:31:28 -0600

    Update Header maker.js

--- Banner maker/Header maker.js
+++ Banner maker/Header maker.js
@@ -2,11 +2,11 @@
 
 var text = document.getElementById("text");
 var preview = document.getElementById("preview")
-var Logo_link; //= "https://resources.finalsite.net/images/f_auto,q_auto,t_image_size_1/v1697702299/d11org/bf4fyds9xl4mdd4ksvac/howbert-logo.png";
-var School_slogan; //="where learning happens beyond four walls";
-var School_name;//="Howbert Elementary";
+var Logo_link; 
+var School_slogan;
+var School_name;
 var borderColor =[0,0,0];
-var out;// '<div style="overflow:hidden;height:fit-content; border5px solid rgb('+borderColor[0]+','+borderColor[1]+','+borderColor[2]+')"><h1 style="text-align:center;">'+School_name+'</h1>\n<div style="text-align:right;display:block;">\n<img src="'+Logo_link+'" style="min-width:10%;max-width:40%;float:right;padding-right:10%;" alt="'+School_name+' Logo"></div>\n<div style="min-width:70%;">\n<h2 style="text-align:center;"> '+School_slogan+'</h2></div><div>';
+var out;
 var theme = 0;
 var themeCount = 10;
 
